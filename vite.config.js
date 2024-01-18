ce4364bc5a13153b3b2c41ed13ed18b4c66c9b76
Hybee <hybee234@outlook.com>
2024-01-19 04:33:42 +1100

    minor updates to pages styling

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const PORT = process.env.PORT || 5173
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: PORT,
+    port: 5173,
     open: true
   }
 })
