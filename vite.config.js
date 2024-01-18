84e0862e4609165436c954787335d4218e337f71
Hybee <hybee234@outlook.com>
2024-01-19 04:47:52 +1100

    heroku ....

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  }
+  // server: {
+  //   port: 5173,
+  //   open: true
+  // }
 })
