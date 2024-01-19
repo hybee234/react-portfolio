70335ce7924bc7ae6c5a414a6cbc2d193ee3c6ee
Hybee <hybee234@outlook.com>
2024-01-19 17:07:17 +1100

    more styling updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
+import { Link } from 'react-router-dom';
+import githublogo from './../assets/images/github logo.png'
+
 function Footer() {
-  return (
-    <div className = "footer">
-      awefawef
-      <a href="https://github.com/hybee234">Link to github</a>
-    </div>
-  );
+    return (
+        <div className = "footer">
+            <div className = "bg-filter">
+                <p>Last updated 19/1/2024</p>
+                <div>
+                    <Link className="border-2" to="https://github.com/hybee234"><img width="30" src={githublogo}></img></Link>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,20 @@
-import picture from './../assets/images/chiikawa.gif'
+import picture from './../assets/images/Profile.jpg'
 
 export default function About() {
     return (
         <div className="page bg-filter">
-            <h1>Huber - About Me</h1>
-            <div>
-                <img className = "m-auto" src={picture}></img>
+            <h1>Hello! I'm Hy </h1>
+                <h1>Welcome to my portfolio!</h1> 
+            <div className = "flex flex-wrap justify-center align-center">
+                <div className = "border-2 p-2">
+                    <img className = "m-auto" src={picture} width="300px"></img>
+                </div>
+                <div className = "border-2 p-2">
+                    <p>
+                        Enthusiastic, budding developer who has just gone through an intense 6 month bootcamp! 
+                    </p>
+                </div>
             </div>
-            <p>
-                Enthusiastic, budding developer who has just gone through an intense 6 month bootcamp! 
-            </p>
         </div>
     );
 }
